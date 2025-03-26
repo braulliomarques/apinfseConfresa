c67cd369d2c39f19b85f61a9d236ddd30e113134
Braullio Marques <160293002+braulliomarques@users.noreply.github.com>
2025-03-26 08:55:30 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,22 +1,24 @@
 import requests
 
+url = "http://172.28.229.102:5000/download_notas"
+
+payload = {
+    "cnpj": "00149146000101",
+    "senha": "Mago@2025",
+    "data_inicio": "01/03/2025",
+    "data_fim": "25/03/2025"
+}
+
 headers = {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json"
 }
 
-json_data = {
-    'cnpj': 'cnpj',
-    'senha': 'senha',
-    'data_inicio': '01/01/2024',
-    'data_fim': '31/03/2025',
-}
+response = requests.post(url, json=payload, headers=headers)
 
-response = requests.post('http://localhost:5000/download_notas', headers=headers, json=json_data)
-
-# Note: json_data will not be serialized by requests
-# exactly as it was in the original request.
-#data = '{\n    "cnpj": "00149146000101",\n    "senha": "SuaSenha",\n    "data_inicio": "01/01/2024",\n    "data_fim": "31/01/2024"\n  }'
-#response = requests.post('http://localhost:5000/download_notas', headers=headers, data=data)
-
-with open('notas.zip', 'wb') as f:
-    f.write(response.content)
+# Verifica se a resposta está ok
+if response.status_code == 200:
+    with open("notas.zip", "wb") as f:
+        f.write(response.content)
+    print("✅ Arquivo salvo como notas.zip")
+else:
+    print(f"❌ Erro {response.status_code}: {response.text}")
